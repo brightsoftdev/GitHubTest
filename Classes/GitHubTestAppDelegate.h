2966745d3c7f25a7842399bc8bc8e03a062bdfe6
Bright Ming <heihate@bogon.(none)>
2011-11-22 14:56:55 +0800

    a test
    just a test

---
+++ Classes/GitHubTestAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  GitHubTestAppDelegate.h
+//  GitHubTest
+//
+//  Created by Bright Ming on 11-11-22.
+//  Copyright 2011 www.zhangyue.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GitHubTestAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  GitHubTest
+//
+//  Created by Bright Ming on 11-11-22.
+//  Copyright 2011 www.zhangyue.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
